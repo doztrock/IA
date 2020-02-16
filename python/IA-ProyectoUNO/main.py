b889d85fa3f9a16a61847b5e485ce162fb0e872c
Ivan Botero <ivan.botero@protonmail.ch>
2020-02-16 13:03:09 -0500

    Se agrega funcionamiento primario

--- python/IA-ProyectoUNO/main.py
+++ python/IA-ProyectoUNO/main.py
@@ -3,24 +3,86 @@ from random import randint
 from datetime import datetime
 
 import tkinter
+from tkinter import messagebox
+from tkinter import simpledialog
+
+''' Rango '''
+MIN_VALUE = 0
+MAX_VALUE = 99
+
+''' Cantidad de intentos '''
+MAX_ATTEMPS = 5
 
 ''' Asignamos la semilla '''
 seed(datetime.now())
 
-'''
-    Funcion:    generateRandomNumber()
-    Objetivo:   Generar un numero aleatorio.
-'''
-def generateRandomNumber(min, max):
-    return randint(min, max)
 
-'''
-    Funcion:    startGame()
-    Objetivo:   Comenzar el juego.
-'''
 def startGame():
-    randomNumber = generateRandomNumber(min = 0, max = 99)
-    print(randomNumber)
+
+    #
+    # Numero pensado
+    #
+    randomNumber = randint(a=MIN_VALUE, b=MAX_VALUE)
+
+    #
+    # Bandera de aprobacion
+    #
+    approved = False
+
+    #
+    # Mensaje inicial
+    #
+    messagebox.showinfo(
+        "¡Antes de empezar!",
+        "A continuacion tendras " + str(MAX_ATTEMPS) + " intentos."
+    )
+
+    #
+    # Depuracion
+    #
+    print("Numero: " + str(randomNumber))
+
+    for counter in range(MAX_ATTEMPS):
+
+        #
+        # Solicitamos la respuesta
+        #
+        answer = simpledialog.askinteger(
+            "Intento #" + str(counter + 1),
+            "¿Que numero he pensado?",
+            minvalue=MIN_VALUE,
+            maxvalue=MAX_VALUE
+        )
+
+        #
+        # Validacion: Respuesta correcta
+        #
+        if answer == randomNumber:
+
+            messagebox.showinfo(
+                "¡Felicitaciones!",
+                "Lo has logrado"
+            )
+
+        #
+        # Validacion: Respuesta menor
+        #
+        if answer < randomNumber:
+
+            messagebox.showerror(
+                "¡Oups!",
+                "He pensado un numero mayor"
+            )
+
+        #
+        # Validacion: Respuesta mayor
+        #
+        if answer > randomNumber:
+
+            messagebox.showerror(
+                "¡Oups!",
+                "He pensado un numero menor"
+            )
 
 
 ''' Ventana '''
@@ -30,10 +92,13 @@ window.geometry("325x130")
 window.resizable(False, False)
 
 ''' Boton '''
-pixel = tkinter.PhotoImage(width = 0, height = 0)
-button = tkinter.Button(window, command = startGame, image = pixel, height = 100, width = 100, compound = "c", text = "Iniciar")
+pixel = tkinter.PhotoImage(width=0, height=0)
+button = tkinter.Button(window,
+                        command=startGame,
+                        image=pixel, height=100, width=100,
+                        compound="c", text="Iniciar")
 button.pack()
-button.place(x = 10, y = 10)
+button.place(x=10, y=10)
 
 ''' Mostramos la ventana '''
 window.mainloop()
