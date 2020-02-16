7afc0b0b2023a335220f646e6bb13c0ebe2a185c
Ivan Botero <ivan.botero@protonmail.ch>
2020-02-16 14:03:19 -0500

    Se agregan cambios en interaccion

--- python/IA-ProyectoUNO/main.py
+++ python/IA-ProyectoUNO/main.py
@@ -30,6 +30,11 @@ def startGame():
     approved = False
 
     #
+    # Contador de intentos
+    #
+    counter = 0
+
+    #
     # Mensaje inicial
     #
     messagebox.showinfo(
@@ -42,7 +47,7 @@ def startGame():
     #
     print("Numero: " + str(randomNumber))
 
-    for counter in range(MAX_ATTEMPS):
+    while counter < MAX_ATTEMPS:
 
         #
         # Solicitamos la respuesta
@@ -54,44 +59,107 @@ def startGame():
             maxvalue=MAX_VALUE
         )
 
-        #
-        # Validacion: Respuesta correcta
-        #
-        if answer == randomNumber:
+        if answer is not None:
+
+            #
+            # Validacion: Respuesta correcta
+            #
+            if answer == randomNumber:
+
+                messagebox.showinfo(
+                    "¡Felicitaciones!",
+                    "Lo has logrado"
+                )
+
+                approved = True
+
+            #
+            # Validacion: Respuesta menor
+            #
+            if answer < randomNumber:
+
+                messagebox.showerror(
+                    "¡Oups!",
+                    "He pensado un numero mayor"
+                )
+
+            #
+            # Validacion: Respuesta mayor
+            #
+            if answer > randomNumber:
+
+                messagebox.showerror(
+                    "¡Oups!",
+                    "He pensado un numero menor"
+                )
+
+        else:
+
+            #
+            # Mensaje de rendicion
+            #
+            if messagebox.askyesno("¿En serio?", "¿Tan rapido te rindes?") == True:
+                break
+            else:
+                counter = counter - 1
+
+        counter = counter + 1
+
+    #
+    # Mensaje de resultado
+    #
+    if approved == True:
+
+        if counter == 0:
 
             messagebox.showinfo(
-                "¡Felicitaciones!",
-                "Lo has logrado"
+                "Resultado",
+                "¡Lo has conseguido, eres una leyenda en el arte de la adivinacion!"
             )
 
-        #
-        # Validacion: Respuesta menor
-        #
-        if answer < randomNumber:
+        elif counter == 1:
 
-            messagebox.showerror(
-                "¡Oups!",
-                "He pensado un numero mayor"
+            messagebox.showinfo(
+                "Resultado",
+                "¡Lo has conseguido, eres un maestro en el arte de la adivinacion!"
             )
 
-        #
-        # Validacion: Respuesta mayor
-        #
-        if answer > randomNumber:
+        elif counter == 2:
 
-            messagebox.showerror(
-                "¡Oups!",
-                "He pensado un numero menor"
+            messagebox.showinfo(
+                "Resultado",
+                "¡Lo has conseguido, eres un experto en el arte de la adivinacion!"
             )
 
+        elif counter == 3:
 
-''' Ventana '''
+            messagebox.showinfo(
+                "Resultado",
+                "¡Lo has conseguido, eres un principiante en el arte de la adivinacion!"
+            )
+
+        elif counter == 4:
+
+            messagebox.showinfo(
+                "Resultado",
+                "¡Lo has conseguido, eres un aprendiz en el arte de la adivinacion!"
+            )
+
+    else:
+        messagebox.showinfo("Resultado", "¡Suerte para la proxima!")
+
+
+#
+# Ventana
+#
 window = tkinter.Tk()
 window.title("Proyecto 1")
 window.geometry("325x130")
 window.resizable(False, False)
 
-''' Boton '''
+#
+# Boton
+#
 pixel = tkinter.PhotoImage(width=0, height=0)
 button = tkinter.Button(window,
                         command=startGame,
@@ -100,5 +168,7 @@ button = tkinter.Button(window,
 button.pack()
 button.place(x=10, y=10)
 
-''' Mostramos la ventana '''
+#
+#  Mostramos la ventana
+#
 window.mainloop()
